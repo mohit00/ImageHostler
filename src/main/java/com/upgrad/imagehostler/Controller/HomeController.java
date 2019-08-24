e800a43e0c160931d20092bafbce74e55471772e
mohit00 <naithanimnaithani@gmail.com>
2019-08-24 11:20:44 +0530

    commit

---
+++ src/main/java/com/upgrad/imagehostler/Controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.upgrad.imagehostler.Controller;
+  import com.upgrad.imagehostler.Model.Image;
+  import com.upgrad.imagehostler.Service.ImageService;
+  import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private ImageService imageService;
+
+    @RequestMapping("/")
+    public String getAllImages(Model model) {
+        List<Image> images = imageService.getAllImages();
+        model.addAttribute("images", images);
+        return "index";
+    }
+}
---
+++ src/main/java/com/upgrad/imagehostler/ImagehostlerApplication.java
@@ -0,0 +1,13 @@
+package com.upgrad.imagehostler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagehostlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImagehostlerApplication.class, args);
+    }
+
+}
